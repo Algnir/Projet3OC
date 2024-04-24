8083c8e92696d74332e193e1a4ae0ec2af5f80d5
Clovis <cpatoue@gmail.com>
2024-04-24 16:40:36 +0200

    ajout des boutons

--- FrontEnd/projet.js
+++ FrontEnd/projet.js
@@ -30,13 +30,13 @@ function genererWorks(works){ // fonction pour générer les photos dans la sect
 
 }
 
-function genererFiltre(categories){
+function genererFiltre(categories){;
     const buttonElement = document.createElement("button");
     buttonElement.id = "0";
     buttonElement.innerText = "Tous";
-    portfolio.insertBefore(buttonElement,gallery);
+    portfolio.insertBefore(buttonElement,gallery);//création du boutton Tous//
 
-    for (let i = 0; i < categories.length; i++) {
+    for (let i = 0; i < categories.length; i++) {//création des bouttons catégories//
         const item = categories[i];
 
         const buttonElement = document.createElement("button");
@@ -52,3 +52,71 @@ function genererFiltre(categories){
 genererWorks(works);
 
 genererFiltre(categories);
+
+
+//fonction des boutons filtres//
+const boutonTous = document.getElementById("0");
+boutonTous.classList.add("clicked");// affichage de Tous par défaut//
+boutonTous.addEventListener("click", function () {
+
+    boutonTous.classList.add("clicked");v
+    boutonAppartements.classList.remove("clicked");
+    boutonObjets.classList.remove("clicked");
+    boutonHotelsetRestaurants.classList.remove("clicked");
+
+    gallery.innerHTML = "";//supression de la galerie sinon on aura les images d'avant qui se rajoute//
+    genererWorks(works);//generation de la galleries//
+});
+
+const boutonObjets = document.getElementById("1");
+boutonObjets.addEventListener("click", function () {
+
+    boutonObjets.classList.add("clicked");// ajout de la class pour le css et supression pour les autres boutons//
+    boutonAppartements.classList.remove("clicked");
+    boutonTous.classList.remove("clicked");
+    boutonHotelsetRestaurants.classList.remove("clicked");
+
+    const objetsWorks = works.filter((work) => work.category.name == "Objets" )//création de la liste avec que objets//
+    gallery.innerHTML = "";//supression de la galerie sinon on aura les images d'avant qui se rajoute//
+    genererWorks(objetsWorks);//affichage de la nouvelle liste//
+});
+
+const boutonAppartements = document.getElementById("2");
+boutonAppartements.addEventListener("click", function () {
+
+    boutonAppartements.classList.add("clicked");// ajout de la class pour le css et supression pour les autres boutons//
+    boutonTous.classList.remove("clicked");
+    boutonObjets.classList.remove("clicked");
+    boutonHotelsetRestaurants.classList.remove("clicked");
+
+    const AppartementsWorks = works.filter((work) => work.category.name == "Appartements" )//création de la liste avec que appartements//
+    gallery.innerHTML = "";//supression de la galerie sinon on aura les images d'avant qui se rajoute//
+    genererWorks(AppartementsWorks);//affichage de la nouvelle liste//
+});
+
+const boutonHotelsetRestaurants = document.getElementById("3");
+boutonHotelsetRestaurants.addEventListener("click", function () {
+
+    boutonHotelsetRestaurants.classList.add("clicked");// ajout de la class pour le css et supression pour les autres boutons//
+    boutonAppartements.classList.remove("clicked");
+    boutonObjets.classList.remove("clicked");
+    boutonTous.classList.remove("clicked");
+
+    const HotelsetRestaurantsWorks = works.filter((work) => work.category.name == "Hotels & restaurants" )//création de la liste avec que hotels et restaurants//
+    gallery.innerHTML = "";//supression de la galerie sinon on aura les images d'avant qui se rajoute//
+    genererWorks(HotelsetRestaurantsWorks);//affichage de la nouvelle liste//
+});
+
+
+
+//page de login//
+
+const login = document.getElementById("login");
+const projet = document.getElementById("login");
+const contact = document.getElementById("login");
+const main = document.getElementById("main");
+login.addEventListener("click", function (){
+    main.classList.add("none");
+
+
+})
